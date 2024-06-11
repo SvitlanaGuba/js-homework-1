e8e135b403d5765e05eb0b343f4fb40f93ee3a17
GubaSvitlana <sg0102sun33@gmail.come>
2024-06-11 13:11:15 +0300

    added new file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    pageLoadTimeout: 120000,//увеличиваем время на загрузку сайта
+    pageLoadTimeout: 150000,//увеличиваем время на загрузку сайта
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
