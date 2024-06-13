fba2f7b93f8164c747fbefb25fc8d6dca46cb21d
GubaSvitlana <sg0102sun33@gmail.come>
2024-06-13 14:17:37 +0300

    added new file- hw 11.06.new

---
+++ cypress/e2e/hw 11.06. new.cy.js
@@ -0,0 +1,110 @@
+describe('testing "Toastr" submenu', () => {
+  beforeEach(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
+    cy.get('[alt="Material Dark Theme"]', {timeout: 20000}).click();
+    cy.get('[title="Modal & Overlays"]').click();
+    cy.get('[title="Toastr"]').click();
+  });
+
+  const testCases = [
+    {
+      testData: {
+        position: 'top-right',
+        title: 'some test1',
+        content: 'some content1',
+        time: '7000',
+        type: 'success'
+      },
+      expectedResult: {
+        icon: 'email',
+        title: 'some test1',
+        content: 'some content1',
+        color: 'rgb(96, 175, 32)',
+        position: 'static'
+      }
+    },
+    {
+      testData: {
+        position: 'top-left',
+        title: 'some test2',
+        content: 'some content2',
+        time: '2000',
+        type: 'warning'
+      },
+      expectedResult: {
+        icon: 'email',
+        title: 'some test2',
+        content: 'some content2',
+        color: 'rgb(255, 190, 67)',
+        position: 'static'
+      }
+    },
+    {
+      testData: {
+        position: 'bottom-right',
+        title: 'some test3',
+        content: 'some content3',
+        time: '3000',
+        type: 'info'
+      },
+      expectedResult: {
+        icon: 'email',
+        title: 'some test3',
+        content: 'some content3',
+        color: 'rgb(64, 187, 244)',
+        position: 'static'
+      }
+    },
+    {
+      testData: {
+        position: 'bottom-left',
+        title: 'some test4',
+        content: 'some content4',
+        time: '4000',
+        type: 'danger'
+      },
+      expectedResult: {
+        icon: 'email',
+        title: 'some test4',
+        content: 'some content4',
+        color: 'rgb(207, 51, 65)',
+        position: 'static'
+      }
+    }
+  ];
+
+
+    testCases.forEach(({testData, expectedResultpassword}) => {
+    it(`Testing cases from 1 to 4`, () => {
+
+      cy.log('Enter fields');
+
+        cy.get('[class="mat-ripple position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"]').click();
+        cy.get(`nb-option[ng-reflect-value="${testData.position}"]`).should("contain", testData.position);
+
+        cy.get('input[name="title"]', {timeout: 2000}).clear({force: true}).type(testData.title, {force: true});
+        cy.get('input[name="content"]', {timeout: 2000}).clear({force: true}).type(testData.content);
+        cy.get('input[name="timeout"]', {timeout: 2000}).clear({force: true}).type(testData.time);
+
+        cy.get('[class="mat-ripple appearance-outline size-medium status-basic shape-rectangle nb-transition"]').click();
+        cy.get(`nb-option-list [ng-reflect-value="${testData.type}"]`).click();
+
+        cy.get('button[class="mat-ripple appearance-filled size-medium shape-rectangle status-basic nb-transition"]').contains('Show toast').click();
+
+        cy.log(`Should display a toast with correct content`);
+
+        cy.get('nb-toast[ng-reflect-toast="[object Object]"]').should('be.visible');
+
+        cy.get('nb-toastr-container nb-toast').then(element =>{
+          expect(element).to.have.css('background-color', 'rgb(96, 175, 32)');
+        })
+
+      cy.get('nb-toastr-container nb-toast').should('have.css', 'background-color', expectedResult.color);
+      cy.get('nb-toastr-container nb-toast div span').should('have.text', expectedResult.title);
+      cy.get('nb-toastr-container nb-toast div [class="message"]').should('have.text', expectedResult.content);
+      cy.get('nb-toast [ng-reflect-config="[object Object]"]').children().should('have.class','data-name')//icon
+      cy.get('nb-toast[ng-reflect-toast="[object Object]"]').should('have.css', 'position', expectedResult.position);
+      })
+
+    })
+})
--- cypress/e2e/hw-11.06.cy.js
+++ cypress/e2e/hw-11.06.cy.js
@@ -7,109 +7,136 @@
          cy.get('[title="Toastr"]').click();
      });
 
-     const testData1 = [
-         {
-             position: 'top-right',
-             title: 'some test1',
-             content: 'some content1',
-             time: '1000',
-             type: 'success'
-         }
-     ];
-
-     const expectedResults1 = [
-         {
-             icon: 'email1',
-             title: 'some test1',
-             content: 'some content1',
-             color: 'rgb(40,167,69)',
-             position: 'static'
-         }
-     ];
-
-     const testData2 = [
-         {
-             position: 'top-left',
-             title: 'some test2',
-             content: 'some content2',
-             time: '2000',
-             type: 'warning'
-         }
-     ];
-
-     const expectedResults2 = [
-         {
-             icon: 'email2',
-             title: 'some test2',
-             content: 'some content2',
-             color: 'rgb(255,193,7)',
-             position: 'static'
-         }
-     ];
-
-     const testData3 = [
-         {
-             position: 'bottom-right',
-             title: 'some test3',
-             content: 'some content3',
-             time: '3000',
-             type: 'info'
-         }
-     ];
-
-     const expectedResults3 = [
-         {
-             icon: 'email3',
-             title: 'some test3',
-             content: 'some content3',
-             color: 'rgb(23,162,184)',
-             position: 'static'
-         }
-     ];
-
-     const testData4 = [
-         {
-             position: 'bottom-left',
-             title: 'some test4',
-             content: 'some content4',
-             time: '4000',
-             type: 'danger'
-         }
-     ];
-
-     const expectedResults4 = [
-         {
-             icon: 'email4',
-             title: 'some test4',
-             content: 'some content4',
-             color: 'rgb(220,53,69)',
-             position: 'static'
-         }
-     ];
-
      const testCases = [
-         {testData: testData1, expectedResults: expectedResults1},
-         {testData: testData2, expectedResults: expectedResults2},
-         {testData: testData3, expectedResults: expectedResults3},
-         {testData: testData4, expectedResults: expectedResults4}
-     ];
+         {
+             testData: {
+                 position: 'top-right',
+                 title: 'some test1',
+                 content: 'some content1',
+                 time: '1000',
+                 type: 'success'
+             },
+             expectedResult: {
+                 icon: 'email1',
+                 title: 'some test1',
+                 content: 'some content1',
+                 color: 'rgb(40,167,69)',
+                 position: 'static'
+             }
+         }
+             ]
+
+
+     // const testData1 = [
+     //     {
+     //         position: 'top-right',
+     //         title: 'some test1',
+     //         content: 'some content1',
+     //         time: '1000',
+     //         type: 'success'
+     //     }
+     // ];
+     //
+     // const expectedResults1 = [
+     //     {
+     //         icon: 'email1',
+     //         title: 'some test1',
+     //         content: 'some content1',
+     //         color: 'rgb(40,167,69)',
+     //         position: 'static'
+     //     }
+     // ];
+     //
+     // const testData2 = [
+     //     {
+     //         position: 'top-left',
+     //         title: 'some test2',
+     //         content: 'some content2',
+     //         time: '2000',
+     //         type: 'warning'
+     //     }
+     // ];
+     //
+     // const expectedResults2 = [
+     //     {
+     //         icon: 'email2',
+     //         title: 'some test2',
+     //         content: 'some content2',
+     //         color: 'rgb(255,193,7)',
+     //         position: 'static'
+     //     }
+     // ];
+     //
+     // const testData3 = [
+     //     {
+     //         position: 'bottom-right',
+     //         title: 'some test3',
+     //         content: 'some content3',
+     //         time: '3000',
+     //         type: 'info'
+     //     }
+     // ];
+     //
+     // const expectedResults3 = [
+     //     {
+     //         icon: 'email3',
+     //         title: 'some test3',
+     //         content: 'some content3',
+     //         color: 'rgb(23,162,184)',
+     //         position: 'static'
+     //     }
+     // ];
+     //
+     // const testData4 = [
+     //     {
+     //         position: 'bottom-left',
+     //         title: 'some test4',
+     //         content: 'some content4',
+     //         time: '4000',
+     //         type: 'danger'
+     //     }
+     // ];
+     //
+     // const expectedResults4 = [
+     //     {
+     //         icon: 'email4',
+     //         title: 'some test4',
+     //         content: 'some content4',
+     //         color: 'rgb(220,53,69)',
+     //         position: 'static'
+     //     }
+     // ];
+
+     // const testCases = [
+     //     {testData: testData1, expectedResults: expectedResults1},
+     //     {testData: testData2, expectedResults: expectedResults2},
+     //     {testData: testData3, expectedResults: expectedResults3},
+     //     {testData: testData4, expectedResults: expectedResults4}
+     // ];
 
      testCases.forEach(({testData, expectedResults}, index) => {
          it(`Verify testData${index + 1}`, () => {
 
-             const positionSelector = `[ng-reflect-selected="${testData[0].position}"]`;
-             const typeSelector = `[ng-reflect-selected="${testData[0].type}"]`;
+             // const positionSelector = `[ng-reflect-selected="${testData[0].position}"]`;
+             // const typeSelector = `[ng-reflect-selected="${testData[0].type}"]`;
 
-             cy.get(positionSelector, {timeout: 10000}).should('have.attr', 'ng-reflect-selected', testData[0].position);
-             cy.get(positionSelector).click({force: true});
+             //cy.get(positionSelector, {timeout: 10000}).should('have.attr', 'ng-reflect-selected', testData[0].position);
+             // cy.get(positionSelector).click({force: true})
+             //cy.get('div.col-md-6.col-sm-12 button.select-button').eq(0).click();
+             //cy.get(`nb-option[ng-reflect-value="${testData[0].position}"]`).click(); ??
 
-             cy.get('input[name="title"]', {timeout: 2000}).clear({force: true}).type(testData[0].title, {force: true});
-             cy.get('input[name="content"]', {timeout: 2000}).clear({force: true}).type(testData[0].content);
-             cy.get('input[name="timeout"]', {timeout: 2000}).clear({force: true}).type(testData[0].time);
+             cy.get('[class="mat-ripple position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"]').click();
+             cy.get('[ng-reflect-selected="top-right"]').click();
+             cy.get(`nb-option[ng-reflect-value="${testData.position}"]`).should("contain", testData.position);
+
+             cy.get('input[name="title"]', {timeout: 2000}).clear({force: true}).type(testData.title, {force: true});
+             cy.get('input[name="content"]', {timeout: 2000}).clear({force: true}).type(testData.content);
+             cy.get('input[name="timeout"]', {timeout: 2000}).clear({force: true}).type(testData.time);
 
 
              cy.get(typeSelector, {timeout: 10000}).should('have.attr', 'ng-reflect-selected', testData[0].type);
              cy.get(typeSelector).click({force: true});
+             //cy.get('div.col-md-6.col-sm-12 button.select-button').eq(0).click();
 
              cy.get('button[class="mat-ripple appearance-filled size-medium shape-rectangle status-basic nb-transition"]').contains('Show toast').click();
 
