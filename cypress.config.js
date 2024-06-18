affbc0fbf877c14775c6d5a74c4c0c5296b139cd
GubaSvitlana <sg0102sun33@gmail.come>
2024-06-18 14:53:25 +0300

    added new files - hw 18.06, helper, user and updated file hw - 11.06.new

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     pageLoadTimeout: 150000,//увеличиваем время на загрузку сайта
     viewportWidth: 1920,
     viewportHeight: 1080,
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
